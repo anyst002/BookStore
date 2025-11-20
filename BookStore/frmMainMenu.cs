80ebbad40525c6da4c799d953b2d2d64f5f8194f
Andrew Nystrom <anyst002@fiu.edu>
2025-11-20 13:29:25 -0500

    Add project files.

---
+++ BookStore/frmMainMenu.cs
@@ -0,0 +1,31 @@
+using System.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace BookStore
+{
+    public partial class frmMainMenu : Form
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["BookStore"].ConnectionString;
+        SqlConnection connection;
+        SqlCommand command;
+
+        public frmMainMenu()
+        {
+            InitializeComponent();
+            connection = new(connectionString);
+            command = new("", connection);
+        }
+
+        public void frmMainMenu_Load(object sender, EventArgs e)
+        {
+            //just for testing the connection, delete/move later
+            connection.Open();
+            command.CommandText = $"SELECT * FROM titles";
+            using SqlDataReader reader = command.ExecuteReader();
+
+            reader.Read();
+            MessageBox.Show(reader.GetString(0));
+            reader.Close();
+        }
+    }
+}
