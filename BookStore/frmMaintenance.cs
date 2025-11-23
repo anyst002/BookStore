a37ffea856d8fba9907b34900e8fcffc352b3b54
akirk63 <ajkirkpatrick36@gmail.com>
2025-11-23 18:25:35 -0500

    Phase 2

--- BookStore/frmMaintenance.cs
+++ BookStore/frmMaintenance.cs
@@ -16,5 +16,34 @@ namespace BookStore
         {
             InitializeComponent();
         }
+        private void btnTitles_Click(object sender, EventArgs e)
+{
+    frmAddTitle addTitle = new frmAddTitle();
+    addTitle.ShowDialog();
+}
+
+private void btnAuthors_Click(object sender, EventArgs e)
+{
+    frmAddAuthor addAuthor = new frmAddAuthor();
+    addAuthor.ShowDialog();
+}
+
+private void btnPublishers_Click(object sender, EventArgs e)
+{
+    frmAddPublisher addPublisher = new frmAddPublisher();
+    addPublisher.ShowDialog();
+}
+
+private void btnStores_Click(object sender, EventArgs e)
+{
+    frmAddStore addStore = new frmAddStore();
+    addStore.ShowDialog();
+}
+
+private void btnEmployees_Click(object sender, EventArgs e)
+{
+    frmAddEmployee addEmployee = new frmAddEmployee();
+    addEmployee.ShowDialog();
+}
     }
 }
